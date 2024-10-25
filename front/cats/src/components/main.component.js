3bc876196c1fc65618bd1323a2a9c9c6359108d6
Margo63 <margo-vinograd@mail.ru>
2024-10-25 12:52:49 +0300

    add test data from catapi

--- front/cats/src/components/main.component.js
+++ front/cats/src/components/main.component.js
@@ -1,6 +1,31 @@
-const MainComponent = () => {
-    return(
-        <div><h3>Main</h3></div>
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {catOptions} from "../options";
+
+export const MainComponent = () => {
+
+
+    const [catData, setCatData] = useState(null);
+    const fetchData = async () => {
+        axios.get("https://api.thecatapi.com/v1/breeds?limit=10&page=0",
+            catOptions)
+            .then(res => {
+                console.log(res);
+                setCatData(res.data)
+            })
+
+            .catch(err => {console.error("error fetching data", err)});
+    }
+    useEffect(() => {
+        fetchData();
+    }, []);
+    return (
+        <div>
+            <h3>Main</h3>
+            {catData?.toString()}
+        </div>
     )
+
 }
+
 export default MainComponent
