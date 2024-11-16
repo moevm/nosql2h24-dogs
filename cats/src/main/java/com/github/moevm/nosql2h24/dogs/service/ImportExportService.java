46cc2710bb84c8ad406d556566d7ceddb4c0412d
Aizomaki <karimmakki002@gmail.com>
2024-11-16 12:44:11 +0300

    add ImportExport server

--- cats/src/main/java/com/github/moevm/nosql2h24/dogs/service/ImportExportService.java
+++ cats/src/main/java/com/github/moevm/nosql2h24/dogs/service/ImportExportService.java
@@ -1,16 +1,21 @@
 package com.github.moevm.nosql2h24.dogs.service;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.moevm.nosql2h24.dogs.database.repository.BreedRepository;
 import com.github.moevm.nosql2h24.dogs.database.repository.EventRepository;
 import com.github.moevm.nosql2h24.dogs.database.repository.UserRepository;
 import com.github.moevm.nosql2h24.dogs.model.controller.Db;
-import org.springframework.stereotype.Service;
 
 @Service
 public class ImportExportService {
 
     private final UserRepository userRepository;
-
     private final BreedRepository breedRepository;
     private final EventRepository eventRepository;
 
@@ -31,5 +36,15 @@ public class ImportExportService {
         breedRepository.saveAll(db.breeds());
         userRepository.saveAll(db.users());
         eventRepository.saveAll(db.events());
+    }
+
+    public void importDbFromFile(MultipartFile file) {
+        try {
+            String content = new String(file.getBytes(), StandardCharsets.UTF_8);
+            Db db = new ObjectMapper().readValue(content, Db.class);
+            importDb(db);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to parse file", e);
+        }
     }
 }
