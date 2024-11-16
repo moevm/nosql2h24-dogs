740dd1729c96c1601319a0de9dc512d69b7571fc
Aizomaki <karimmakki002@gmail.com>
2024-11-16 12:43:21 +0300

    add ImportExport controller

--- cats/src/main/java/com/github/moevm/nosql2h24/dogs/controller/ImportExportController.java
+++ cats/src/main/java/com/github/moevm/nosql2h24/dogs/controller/ImportExportController.java
@@ -1,10 +1,18 @@
 package com.github.moevm.nosql2h24.dogs.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.github.moevm.nosql2h24.dogs.model.controller.Db;
 import com.github.moevm.nosql2h24.dogs.service.ImportExportService;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -21,9 +29,14 @@ public class ImportExportController {
         return importExportService.getImportExport();
     }
 
-    @PostMapping("/import")
-    public HttpStatus importDb(@RequestBody Db db) {
-        importExportService.importDb(db);
-        return HttpStatus.OK;
+    @PostMapping(value = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public HttpStatus importDbFromFile(@RequestParam("file") MultipartFile file) {
+        try {
+            importExportService.importDbFromFile(file);
+            return HttpStatus.OK;
+        } catch (Exception e) {
+            log.error("File import failed: {}", e.getMessage());
+            return HttpStatus.BAD_REQUEST;
+        }
     }
 }
