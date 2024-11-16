a23932d15a34c2fecfec9258efb282cf0ca5ad90
Aizomaki <karimmakki002@gmail.com>
2024-11-16 12:46:28 +0300

    add ImportExport web page to react

--- front/cats/src/App.js
+++ front/cats/src/App.js
@@ -1,33 +1,29 @@
-
-
-import { BrowserRouter as Router, Route, Routes,Navigate    } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import StatisticComponent from "./components/statistic.component";
 import CatCardComponent from "./components/catCard.component";
 import MainComponent from "./components/main.component";
 import ProfileComponent from "./components/profile.component";
 import SignInComponent from "./components/signIn.component";
 import SignUpComponent from "./components/signUp.component";
+import ImpExpComponent from "./components/ImpExpComponent";
 
 function App() {
   return (
-      <Router>
-          <div>
-              <Routes>
-                  <Route exact path="/cat_card" element={<CatCardComponent/>}/>
-                  <Route exact path="/main" element={<MainComponent/>}/>
-                  <Route exact path="/profile" element={<ProfileComponent/>}/>
-                  <Route exact path="/sign_in" element={<SignInComponent/>}/>
-                  <Route exact path="/sign_up" element={<SignUpComponent/>}/>
-                  <Route exact path="/statistic" element={<StatisticComponent/>}/>
-                  <Route path="*" element={<Navigate to="/sign_in" replace={true} />}/>
-              </Routes>
-          </div>
-      </Router>
-
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/cat_card" element={<CatCardComponent />} />
+          <Route exact path="/main" element={<MainComponent />} />
+          <Route exact path="/profile" element={<ProfileComponent />} />
+          <Route exact path="/sign_in" element={<SignInComponent />} />
+          <Route exact path="/sign_up" element={<SignUpComponent />} />
+          <Route exact path="/statistic" element={<StatisticComponent />} />
+          <Route exact path="/test" element={<ImpExpComponent />} />
+          <Route path="*" element={<Navigate to="/sign_in" replace={true} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-function NoMatch() {
-    return <div><h3>No match!</h3></div>
-}
 export default App;
