6795f7e1c7f1b7778d6e66e52581bad89ff7bdb8
Margo63 <margo-vinograd@mail.ru>
2024-12-05 14:08:37 +0300

    style bubble

--- front/cats/src/ui/commentBubble.component.js
+++ front/cats/src/ui/commentBubble.component.js
@@ -2,6 +2,7 @@ import React, {useRef} from 'react';
 import CommentTextField from "./commentTextField.js";
 import {useDispatch} from "react-redux";
 import {setAuthor} from "../slice/dataSlice";
+import {Favorite} from "@mui/icons-material";
 
 const CommentComponent=(props)=>{
     const creation_date =  new Date(props.date);
@@ -10,9 +11,21 @@ const CommentComponent=(props)=>{
         <div className="comment_bubble" onClick={()=>{
             dispatch(setAuthor(props.author))
         }}>
-            <label>{props.author}</label>
-            <label>{creation_date.getDate()}.{creation_date.getMonth()+1}.{creation_date.getFullYear()} {creation_date.getHours()}:{creation_date.getMinutes()}</label>
-            <label>{props.text}</label>
+            <div className="comment_bubble_author_text">
+                <label className="small_text">{props.author}: </label>
+                <label className="small_text">{props.text}</label>
+            </div>
+
+            <div className="comment_bubble_column">
+                <label>{creation_date.getDate()}.{creation_date.getMonth()+1}.{creation_date.getFullYear()}
+                    {creation_date.getHours()}:{creation_date.getMinutes()}</label>
+                <div className="comment_bubble_likes">
+                    <Favorite></Favorite>
+                    <label>10</label>
+                </div>
+            </div>
+
+
         </div>
     )
 }
