87e1cf89a9ec69808acd277600d57cc554def068
Margo63 <margo-vinograd@mail.ru>
2024-12-06 14:24:54 +0300

    edit pre-done

--- front/cats/src/components/edit_profile.component.js
+++ front/cats/src/components/edit_profile.component.js
@@ -1,28 +1,91 @@
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {useNavigate} from "react-router-dom";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {Button, IconButton, TextField} from "@mui/material";
 import {ArrowBack, Equalizer, Face, Settings} from "@mui/icons-material";
-import {clearUserData} from "../slice/userSlice";
+import {clearUserData, setUserData} from "../slice/userSlice";
 import SmallCatCardComponent from "../ui/small_cards/smallCatCard.component";
 import DeleteCatCardComponent from "../ui/small_cards/deleteCatCardComponent";
+import axios, {put} from "axios";
+import {BASE_URL} from "../options";
 
 const EditProfileComponent=(props)=>{
+    let user = useSelector(state => state.user);
     let user_data = JSON.parse(localStorage.getItem("userData"));
     const favorites = user_data.favorites;
+
     const imgRef = useRef();
     const navigate = useNavigate();
     let dispatch = useDispatch();
     const [name,setName]=useState(user_data.name);
     const [age,setAge]=useState(user_data.age);
+    const [password,setPassword]=useState(user_data.passwordHash);
     const [img,setImg]=useState(user_data.image);
-    const favorite_data =
-        favorites?.map(fav =>
-            <div>
-                <DeleteCatCardComponent id={fav}
-                ></DeleteCatCardComponent>
-            </div>
-        )
+    const [favoritesData, setFavoritesData]=useState([]);
+    //
+    useEffect(()=>{
+        console.log(user_data)
+        dispatch(setUserData(user_data));
+        setFavoritesData(favorites)
+    },[])
+
+    const putAge = async () => {
+
+        axios.put(BASE_URL + "/users/age/"+age+"/"+user_data?.name, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+                user_data.age=age;
+                localStorage.setItem("userData",JSON.stringify(user_data))
+                dispatch(setUserData(user_data))
+            })
+
+            .catch(err => {
+                console.error("error fetching data", err)
+            });
+    }
+    const putPassword = async () => {
+
+        axios.put(BASE_URL + "/users/passwordHash/"+password+"/"+user_data?.name, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+                user_data.passwordHash=password;
+                localStorage.setItem("userData",JSON.stringify(user_data))
+                dispatch(setUserData(user_data))
+            })
+
+            .catch(err => {
+                console.error("error fetching data", err)
+            });
+    }
+    const putImage = async () => {
+
+        axios.put(BASE_URL + "/users/image/"+img+"/"+user_data?.name, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+                user_data.image=img;
+                localStorage.setItem("userData",JSON.stringify(user_data))
+                dispatch(setUserData(user_data))
+            })
+
+            .catch(err => {
+                console.error("error fetching data", err)
+            });
+    }
     return(
         <div className="profile_box">
             <div className="profile_data_row">
@@ -43,6 +106,13 @@ const EditProfileComponent=(props)=>{
                             onChange={(e) => setAge(e.target.value)}
                         ></TextField>
                     </div>
+                    <div className="small_text">Password:
+                        <TextField
+                            className="big_text"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        ></TextField>
+                    </div>
                     <div className="small_text">Image:
                         <TextField
                             className="big_text"
@@ -55,9 +125,18 @@ const EditProfileComponent=(props)=>{
 
             <label className="medium_text">Favorites</label>
             <div className="card_row">
-                {favorite_data}
+                {user?.favorites?.map(fav =>
+                    <div>
+                        <DeleteCatCardComponent id={fav}
+                        ></DeleteCatCardComponent>
+                    </div>
+                )}
             </div>
             <button onClick={() => {
+                putAge()
+                putPassword()
+                putImage()
+
                 navigate("/profile")
             }} className="button">SAVE</button>
         </div>
--- front/cats/src/components/profile.component.js
+++ front/cats/src/components/profile.component.js
@@ -19,7 +19,7 @@ const ProfileComponent = () => {
     let dispatch = useDispatch();
     //let user_data = useSelector(state => state.user);
     let user_data = JSON.parse(localStorage.getItem("userData"));
-
+    let user = useSelector(state => state.user);
     //alert(JSON.stringify(user_data))
     const favorites = user_data.favorites;
     const [favoritesData, setFavoritesData] = useState([]);
@@ -57,15 +57,68 @@ const ProfileComponent = () => {
     const navigate = useNavigate();
     //const [catData, setCatData] = useState(null);
 
+    const getNotifications = async () => {
+
+        axios.get(BASE_URL + "/events/notifications/"+user_data?.name, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+            })
+
+            .catch(err => {
+                console.error("error fetching data", err)
+            });
+    }
+    const getLikes = async () => {
+
+        axios.get(BASE_URL + "/events/likes/"+user_data?.name, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+            })
+
+            .catch(err => {
+                console.error("error fetching data", err)
+            });
+    }
+    const getComments = async () => {
+
+        axios.get(BASE_URL + "/events/comments/"+user_data?.name, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+            })
+
+            .catch(err => {
+                console.error("error fetching data", err)
+            });
+    }
 
     useEffect(()=>{
         if(Object.keys(user_data).length === 0)
             navigate("/sign_in")
-    });
+        dispatch(setUserData(user_data));
+        getNotifications()
+        getComments()
+        getLikes()
+
+    },[]);
 
 
-    const creation_date =  new Date(user_data.creationDate);
-    const last_date =  new Date(user_data.lastDate);
+    const creation_date =  new Date(user.creationDate);
+    const last_date =  new Date(user.lastDate);
 
     const favorite_data =
         favorites?.map(fav =>
@@ -133,8 +186,8 @@ const ProfileComponent = () => {
                     <Face/>
                 }/>
                 <div className="profile_data_column">
-                    <div className="big_text">{user_data.name}</div>
-                    <div className="small_text">Age: {user_data.age}</div>
+                    <div className="big_text">{user.name}</div>
+                    <div className="small_text">Age: {user.age}</div>
                     <div className="small_text">Creation date: {creation_date.getDate()}.{creation_date.getMonth()+1}.{creation_date.getFullYear()} {creation_date.getHours()}:{creation_date.getMinutes()}</div>
                     <div className="small_text">Last edit date: {last_date.getDate()}.{last_date.getMonth()+1}.{last_date.getFullYear()} {last_date.getHours()}:{last_date.getMinutes()}</div>
                 </div>
